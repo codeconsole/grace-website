580199ad1f97ae4ae08fbd57b8e2d68c6e698188
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-05-31 10:23:54 -0700

    2023.0.0-M6

---
+++ app/domain/website/Sample.groovy
@@ -0,0 +1,11 @@
+package website
+
+class Sample {  
+
+    String prop1
+    Integer prop2
+    Date prop3
+
+    static constraints = {
+    }
+}
---
+++ app/init/website/BootStrap.groovy
@@ -0,0 +1,11 @@
+package website
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+
+    def destroy = {
+    }
+
+}
